c71101b92ea04f00933abe06a9ae3c9bebe11327
yusufyaman07 <yamanyusuf00000@gmail.com>
2024-05-17 23:39:08 +0300

    projeye devam edildi

--- gmail-clone/js/main.js
+++ gmail-clone/js/main.js
@@ -1,5 +1,6 @@
 // console.log("Bağlantı kontrolü");
-import { showModal } from "./ui.js";
+import { showModal, renderMails } from "./ui.js";
+import { months } from "./contants.js";
 //! Html'den Gelen Elemmanlar
 const body = document.querySelector("body");
 const btn = document.getElementById("toggle");
@@ -13,6 +14,18 @@ const mailsArea = document.querySelector(".mails-area");
 const searchButton = document.querySelector("#search-icon");
 const searchInput = document.querySelector("#search-input");
 const categoryArea = document.querySelector(".nav-middle");
+
+// Sayfa yüklenince mailleri render eden fonk.
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderMails(mailsArea, mailData);
+});
+
+// localden veri alma
+const strMailData = localStorage.getItem("data");
+// Mail Dizisi
+const mailData = JSON.parse(strMailData) || [];
+
 // Olay İzleyicileri
 hamburgerMenu.addEventListener("click", hideMenu);
 
@@ -24,3 +37,118 @@ function hideMenu() {
 // Modal Alanı ile ilgili işlemler
 createMailBtn.addEventListener("click", () => showModal(modal, true));
 closeMailBtn.addEventListener("click", () => showModal(modal, false));
+
+// Ekran boyutuna bağlı olarak gerçekleşen işlemler
+
+/*
+Ekran boyutuna bağlı olarak navigation kısımı için hide klası ekleyip çıkardık.
+
+Bu sayede ekran boyutu 1000'in altına düşünce bu kısım gizlendi(hide clası ekledik)
+1000'in üzerinde ise bu yapı görünür oldu.(hide clasını kaldırdırdık.)
+
+
+*/
+
+window.addEventListener("resize", (e) => {
+  const width = e.target.innerWidth;
+  if (width < 1000) {
+    navigation.classList.add("hide");
+  } else {
+    navigation.classList.remove("hide");
+  }
+});
+
+// Mail İşlemleri:
+
+form.addEventListener("submit", sendMail);
+// Tarih Oluşturan Fonk.
+function getDate() {
+  // New Date ile elde edilen tarihin gün ve ayını değişkenlere aktararak istenilen yapıyı elde edeceğiz.
+
+  const today = new Date();
+  const day = today.getDate();
+  const ay = today.getMonth();
+
+  // Tarih Verisi Elde etme
+  const date = day + "/" + ay + "year";
+  const updateMonths = months[ay];
+  return day + " " + updateMonths;
+}
+
+// Mail Oluşturan Fonk.
+function sendMail(e) {
+  // Sayfa Yenileme İptal
+  e.preventDefault();
+  const receiver = e.target[0].value;
+  const title = e.target[1].value;
+  const message = e.target[2].value;
+  // console.log(receiver,title,message);
+
+  if (!receiver || !title || !message) {
+    Toastify({
+      text: "Formu Doldurunuz",
+      duration: 3000,
+      destination: "https://github.com/apvarun/toastify-js",
+      newWindow: true,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "right", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "#FF5733",
+        borderRadius: "10px",
+      },
+      onClick: function () {}, // Callback after click
+    }).showToast();
+    // Buradaki yapılardan herhangi biri boş ise kodu kırar ve geri kalan kısımı çalıştırmaz
+    return;
+  }
+
+  const newMail = {
+    id: new Date().getTime(), // id'ler benzersiz yapıda olmak durumundadır.Bizde burada milisaniye cinsinden benzersiz bir değer elde ettik
+    sender: "Yusuf",
+    receiver,
+    title,
+    message,
+    // stared,
+    date: getDate(),
+  };
+  // Diziye mail eklendi
+  mailData.unshift(newMail);
+  // localstorage verileri kaybetmemizi engeller bundan dolayı bu yapıyı kullanalım
+
+  // localstorage string değer kabul eder bundan dolayı elimizdeki veriyi uygun formata çevirdik.
+  const strData = JSON.stringify(mailData);
+
+  // localstorage' a veri kaydet
+  localStorage.setItem("data", strData);
+  // modalı kapatıldı
+  showModal(modal, false);
+  // Modalı temizle
+  e.target[0].value = "";
+  e.target[1].value = "";
+  e.target[2].value = "";
+  Toastify({
+    text: "Mail Başarıyla Eklendi",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+  // Ekranı güncelle
+  renderMails(mailsArea, mailData);
+}
+
+// Toggle Yapısı
+
+btn.addEventListener("click", () => {
+  btn.classList.toggle("active");
+  btn.classList.toggle("darkMode");
+});
--- gmail-clone/js/ui.js
+++ gmail-clone/js/ui.js
@@ -1,5 +1,42 @@
 // Burada bir fonksiyon tanımladık.Bu fonksiyon alınan parametreye göre modal'ın styleını belirliyor.Verilen 'willOpen' değeri true ise yapıyı gösterip false ise bu yapının display'ını none şeklinde belirlemiş oluyoruz.
 
+//!Modal Aç/Kapa Yapan Fonk.
 export function showModal(modal, willOpen) {
   modal.style.display = willOpen ? "grid" : "none";
+}
+
+//!Metin kısıtlaması yapan fonk.
+function trimString(str, max) {
+  // Burada fonk. verilen metin max. karakter sayısından küçük ise bu yapıyı olduğu şekilde return et
+  if (str.length < max) return str;
+  // Verilen metin max. karakter sayısından büyük ise max. karakter kısımını aşan yapıyı kes ve '...' ile birleştir.
+  return str.slice(0, max) + "...";
+}
+
+//!Mailleri ekrana basan Fonk.
+export function renderMails(outlet, data) {
+  if (!data) return;
+  // bu fonk. her bir mail objesi için bir maili temsil eden html oluştursun.
+  // Yani herbir maili ekrana bassın.
+  outlet.innerHTML = data
+    .map(
+      (mail) => `
+  <div class="mail" id="mail" data-id=${mail.id} >
+   <div class="left">
+   <input type="checkbox" />
+    <span><i class="bi bi-star"></i></span>
+    <span>${mail.receiver} </span>
+     </div>
+    <div class="right">
+    <p class="message-title">${trimString(mail.title, 20)} </p>
+    <p class="message-description">${trimString(mail.message, 40)} </p>
+    <p class="message-date">${mail.date} </p>
+     <div class="delete">
+    <i class="bi bi-trash"></i>
+    </div>
+    </div>
+   </div>
+  `
+    )
+    .join(" ");
 }
